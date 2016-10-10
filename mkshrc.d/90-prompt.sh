6e8400a968a98d896764257eee211b1063bc3300
Duncaen <mail@duncano.de>
2016-10-10 16:05:15 +0200

    mksh: add host name to prompt for ssh connections

--- mkshrc.d/90-prompt.sh
+++ mkshrc.d/90-prompt.sh
@@ -1,1 +1,1 @@
-PS1='$(d=${PWD/$HOME/\~};print ${d##*/})$(((r=$?))&&print "?\e[0;31;40m$r\e[0m")$(((USER_ID))&&print \$||print \#) '
+PS1='$(d=${PWD/$HOME/\~};print ${d##*/})$(((r=$?))&&$([ -n "$SSH_CONNECTION" ] && print "@${HOSTNAME:=$(hostname)}"||true)&&print "?\e[0;31;40m$r\e[0m")$(((USER_ID))&&print \$||print \#) '
