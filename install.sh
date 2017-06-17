58e9c5e42e47c482fb5ca4d45c9c5f7fb4cfecde
Duncaen <mail@duncano.de>
2017-06-17 21:00:15 +0200

    install: avoid symlink loop

--- install.sh
+++ install.sh
@@ -5,6 +5,7 @@ msg() { t=$1; shift; printf '%s: %s\n' "$t" "$@"; }
 _ln_home() {
 	[ -z "$2" ] && tgt="$HOME/$1" || tgt="$HOME/$2"
 	src="$PWD/$1"
+	[ -e "$tgt.bak" ] && rm "$tgt.bak"
 	[ -e "$tgt" ] && mv "$tgt" "$tgt.bak"
 	ln -sf "$src" "$tgt"
 }
