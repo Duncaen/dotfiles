c6db9b6dfe23535329cec33d471b218527b535f6
Duncan Overbruck <mail@duncano.de>
2026-03-10 19:54:44 +0100

    remove unused stuff

--- install-root.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-
-install -d /etc/sv/runsvdir-duncan
-install -m755 sv/runsvdir-duncan/run /etc/sv/runsvdir-duncan/run
-install -m755 sv/runsvdir-duncan/finish /etc/sv/runsvdir-duncan/finish
-ln -sf /run/runit/supervise.runsvdir-duncan /etc/sv/runsvdir-duncan/supervise
